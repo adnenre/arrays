1225caf4e07bdd4975687a32908d02f2fb5e5ed4
adnen rebai <adnen.rebai@gmail.com>
2018-06-24 01:37:48 +0200

    add solution and test for array.shift(array)

--- index.js
+++ index.js
@@ -46,9 +46,20 @@ function reduceRight(array) {}
 
 function reverse(array) {}
 
-function shift(array) {}
+function shift(array) {
+  const res = array[0];
+  
+    for(let i = 0; i<array.length-1;i++){
+      array[i]=array[i+1];
+    }
+    array.length = array.length-1;
+  return res;
+}
 
-function slice(array) {}
+
+function slice(array) {
+  
+}
 
 function some(array) {}
 
--- index.test.js
+++ index.test.js
@@ -13,3 +13,12 @@ test('push', () => {
   expect(length).toBe(3);
   expect(a).toEqual([1, 2, 4]);
 });
+
+test('shift',() =>{
+  const a = ['a','b','c','d'];
+  const result = array.shift(a);
+  expect(result).toBe('a');
+  expect(a).toEqual(['b','c','d']);
+});
+
+
